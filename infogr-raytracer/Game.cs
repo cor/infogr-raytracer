a61f9e009c751e87c58d04b7c915c3989a9cdd00
cor <cor@pruijs.nl>
2020-05-13 14:39:28 +0200

    Let the texture fill the screen

--- infogr-raytracer/Game.cs
+++ infogr-raytracer/Game.cs
@@ -12,10 +12,10 @@ namespace infogr_raytracer
         private float[] _vertices =
         {
             // Position         Texture coordinates
-             0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
-             0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
-            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
-            -0.5f,  0.5f, 0.0f, 0.0f, 1.0f  // top left
+             1.0f,  1.0f, 0.0f, 1.0f, 1.0f, // top right
+             1.0f, -1.0f, 0.0f, 1.0f, 0.0f, // bottom right
+            -1.0f, -1.0f, 0.0f, 0.0f, 0.0f, // bottom left
+            -1.0f,  1.0f, 0.0f, 0.0f, 1.0f  // top left
         };
         
         private readonly uint[] _indices =
