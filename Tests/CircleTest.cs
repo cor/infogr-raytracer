091e3198716d27c09023ee280e3fe42359200184
kaiserkarel <k.l.kubat@gmail.com>
2020-05-21 16:28:58 +0200

    Fix: Add untracked files for Circle.Intersects unit tests.

--- Tests/CircleTest.cs
+++ Tests/CircleTest.cs
@@ -1,4 +1,30 @@
-$HEADER$namespace $NAMESPACE$
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using infogr_raytracer;
+using OpenTK;
+
+namespace Tests
 {
-  public class $CLASS$ {$END$}
+    public class CircleTest
+    {
+        private static Circle circle = new Circle()
+        {
+            Position = new Vector2(2f,2f),
+            Radius = 1f,
+        };
+        
+        private static List<(Ray, bool, string)> cases = new List<(Ray, bool, string)>()
+        {
+            (new Ray() {Origin = new Vector2(0, 2), Direction = new Vector2(1, 0)}, true, "A"),
+            (new Ray() {Origin = new Vector2(0, 2), Direction = new Vector2(1, 0)}, true, "B")
+        };
+        
+        [Test]
+        [TestCaseSource("cases")]
+        public void TestIntersect((Ray, bool, string) testCase)
+        {
+            Assert.AreEqual(testCase.Item2, circle.Intersects(testCase.Item1), testCase.Item3);
+        }
+    }
 }
