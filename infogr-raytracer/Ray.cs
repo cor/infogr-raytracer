ad79d91c74e61a8dd9293eef7ea9b6ffd80c4f25
cor <cor@pruijs.nl>
2020-05-21 12:58:36 +0200

    Automatically normalize Ray direction vectors

--- infogr-raytracer/Ray.cs
+++ infogr-raytracer/Ray.cs
@@ -5,7 +5,14 @@ namespace infogr_raytracer
     public struct Ray
     {
         public Vector2 Origin;
-        public Vector2 Direction;
+
+        private Vector2 _direction;
+        public Vector2 Direction
+        {
+            get => _direction;
+            set => _direction = value.Normalized();
+        }
+        
         public float T;
     }
 }
