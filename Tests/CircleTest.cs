f766d8acb45f4718325335b0be7e2e842623124e
cor <cor@pruijs.nl>
2020-05-21 17:21:40 +0200

    Shoot rays from the inside of a circle

--- Tests/CircleTest.cs
+++ Tests/CircleTest.cs
@@ -120,6 +120,27 @@ namespace Tests
                 Ray = new Ray() { Origin = new Vector2(6, 0), Direction = new Vector2(0, 1)},
                 ExpectedResult = false,
                 Description = "Ray that flies to the right of the circle"
+            },
+            new CircleTestCase()
+            {
+                Circle = new Circle() { Position = new Vector2(4, 4), Radius = 1f},
+                Ray = new Ray() { Origin = new Vector2(4, 4), Direction = new Vector2(0, 1)},
+                ExpectedResult = true,
+                Description = "Ray from the inside the circle"
+            },
+            new CircleTestCase()
+            {
+                Circle = new Circle() { Position = new Vector2(4, 4), Radius = 0.1f},
+                Ray = new Ray() { Origin = new Vector2(4, 4), Direction = new Vector2(0, 1)},
+                ExpectedResult = true,
+                Description = "Ray from the inside of a smaller the circle"
+            },
+            new CircleTestCase()
+            {
+                Circle = new Circle() { Position = new Vector2(4, 4), Radius = 0.001f},
+                Ray = new Ray() { Origin = new Vector2(4, 4), Direction = new Vector2(0, 1)},
+                ExpectedResult = true,
+                Description = "Ray from the inside of a tiny circle"
             }
         };
         
